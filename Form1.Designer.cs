8db88417f1674842b87cf90d6da49e2a765a1a8c
shunrexd <72516739+shunrexd@users.noreply.github.com>
2021-05-23 20:20:30 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace lobbycrasher31
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(12, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 72);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "click to crash";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(145, 211);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 71);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "fix client";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(235, 175);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(49)))), ((int)(((byte)(54)))));
+            this.ClientSize = new System.Drawing.Size(262, 295);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "lobby crasher | shunre#7331";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Net.Http;
+using System.Windows.Forms;
+using System.Security.Cryptography.X509Certificates;
+using System.Net;
+using Microsoft.Win32;
+using System.Net.Security;
+using System.IO;
+using lobbycrasher31.webhook;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace lobbycrasher31
+{
+	public partial class Form1 : Form
+    {
+		private LCUListener listener = new LCUListener();
+
+		private IContainer components = null;
+
+
+		public Form1()
+		{
+			InitializeComponent();
+			listener.StartListening();
+		}
+
+		private async void pictureBox1_Click(object sender, EventArgs e)
+        {
+		}
+
+		private async void button1_Click(object sender, EventArgs e)
+		{
+                if (this.listener.GetGatheredLCUs().Count == 0)
+                {
+					MessageBox.Show("Please run League of Legends first");
+				}
+                else
+                {
+					foreach (LCUClient LCU in this.listener.GetGatheredLCUs())
+					{
+
+
+					await LCU.HttpDelete("/lol-lobby/v2/lobby");
+					await LCU.HttpPostJson("/lol-lobby/v2/matchmaking/quick-search", "{\"queueId\":2000}");
+				}
+				}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+        {
+			this.FormClosing += Form1_FormClosing;
+		}
+
+		private async void button2_Click_1(object sender, EventArgs e)
+        {
+			if (this.listener.GetGatheredLCUs().Count == 0)
+			{
+				MessageBox.Show("Please run League of Legends first");
+			}
+            else
+            {
+
+				foreach (LCUClient LCU in listener.GetGatheredLCUs())
+				{
+					await LCU.HttpPostForm("/lol-login/v1/session/invoke", new List<KeyValuePair<string, string>>
+			{
+				new KeyValuePair<string, string>("destination", "gameService"),
+				new KeyValuePair<string, string>("method", "quitGame"),
+				new KeyValuePair<string, string>("args", "[]")
+			});
+					await LCU.HttpDelete("/lol-lobby/v2/lobby");
+					await LCU.HttpPostJson("/lol-lobby/v2/lobby", "{\"queueId\":430}");
+					await LCU.HttpPostJson("/lol-lobby/v2/lobby/matchmaking/search", null);
+					await LCU.HttpDelete("/lol-lobby/v2/lobby/matchmaking/search");
+				}
+			}
+
+
+		}
+		protected override void Dispose(bool disposing)
+		{
+			bool flag = disposing && this.components != null;
+			if (flag)
+			{
+				this.components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			this.listener.StopListening();
+		}
+
+    }
+}
